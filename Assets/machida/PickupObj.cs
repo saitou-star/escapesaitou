e5d5f6b4fcc5406669d5712860b3de677552944a
yamasabu30 <barani25510@gmail.com>
2023-12-12 12:54:54 +0900

    12:54

---
+++ Assets/machida/PickupObj.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObj : MonoBehaviour
+{
+    　　　//クリックしたら消える(取得)
+    public void OnClickObj() 
+    {
+        gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
