5204e4da0374427a1f71ead0a48fa018f7466376
yamasabu30 <barani25510@gmail.com>
2023-12-17 22:59:08 +0900

    自宅

---
+++ Assets/machida/Save.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveButton : MonoBehaviour
+{
+    public void OnSaveButtonClick()
+    {
+        // セーブ処理を呼ぶ
+        // SaveManager.Instance.SaveGame();
+
+        // セーブ画面に切り替える
+        SceneManager.LoadScene("SaveScene"); // "SaveScene"は適切なシーン名に変更
+    }
+}
