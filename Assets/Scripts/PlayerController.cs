78548e281d5ab5ebdf69054e544217cf1d4a82fa
Rina-24 <rina.k.game@gmail.com>
2023-12-18 09:24:30 +0900

    ペンギン修正

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,100 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     [SerializeField]
+//     // 移動速度(仮)
+//     private float _speed = 10.0f;
+//     // x軸方向の入力を保存
+//     private float _input_x;
+//     // z軸方向の入力を保存
+//     private float _input_z;
+//     // プレイヤーのTransformコンポーネント
+//     private Transform _playerTransform;
+//     private Animator _penguinAnimator;
+//     private Rigidbody _rb;
+
+//     void Start()
+//     {
+//         _rb = GetComponent<Rigidbody>();
+//         _playerTransform = GetComponent<Transform>();
+//         _penguinAnimator = GetComponent<Animator>();
+//     }
+
+//     void Update()
+//     {
+//         // x軸方向、z軸方向の入力を取得
+//         // Horizontal（水平、横方向）
+//         _input_x = Input.GetAxis("Horizontal");
+//         // Vertical（垂直、縦方向
+//         _input_z = Input.GetAxis("Vertical");
+
+//         Vector3 velocity = new Vector3(_input_x, 0, _input_z);
+//         // ベクトルの向きを取得
+//         Vector3 direction = velocity.normalized;
+//         // 移動距離を計算
+//         float distance = _speed * Time.deltaTime;
+//         // Rigidbodyに速度情報を伝えて移動
+//         _rb.velocity = new Vector3(direction.x * _speed, _rb.velocity.y, direction.z * _speed);
+
+//         TurnPlayer();
+//         UpdateAnimator();
+//     }
+
+//     // 左右の向きの調整
+//     void TurnPlayer()
+//     {
+//         if (_input_x > 0)
+//         {
+//             _playerTransform.localScale = new Vector3(-1, 1, 1);
+//         }
+//         else if (_input_x < 0)
+//         {
+//             _playerTransform.localScale = new Vector3(1, 1, 1);
+//         }
+//     }
+
+//     void UpdateAnimator()
+//     {
+//         float moveSpeed = Mathf.Abs(_input_x) + Mathf.Abs(_input_z);
+
+//         // キーボードが押されているかどうかを確認
+//         bool isWalking = moveSpeed > 0;
+
+//         // アニメーションの制御
+//         _penguinAnimator.SetBool("Idle", !isWalking);
+//         _penguinAnimator.SetBool("Walk_Side", false);
+//         _penguinAnimator.SetBool("Walk_Front", false);
+//         _penguinAnimator.SetBool("Walk_Back", false);
+
+//         if (isWalking)
+//         {
+//             if (_input_x < 0)
+//             {
+//                 // 左に移動
+//                 _penguinAnimator.SetBool("Walk_Side", true);
+//             }
+//             else if (_input_x > 0)
+//             {
+//                 // 右に移動
+//                 _penguinAnimator.SetBool("Walk_Side", true);
+//             }
+//             else if (_input_z < 0)
+//             {
+//                 // 後ろに移動
+//                 _penguinAnimator.SetBool("Walk_Front", true);
+//             }
+//             else if (_input_z > 0)
+//             {
+//                 // 前に移動
+//                 _penguinAnimator.SetBool("Walk_Back", true);
+//             }
+//         }
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,56 +102,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    // 移動速度(仮)
-    private float _speed = 10.0f;
-    // x軸方向の入力を保存
-    private float _input_x;
-    // z軸方向の入力を保存
-    private float _input_z;
-    // プレイヤーのTransformコンポーネント
+    private float _speedPG = 10.0f;
+    private float _inputx;
+    private float _inputy;
     private Transform _playerTransform;
     private Animator _penguinAnimator;
-    private Rigidbody _rb;
+    private Rigidbody _rigidbody;
+    private AudioSource playerAudioSource;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
         _playerTransform = GetComponent<Transform>();
         _penguinAnimator = GetComponent<Animator>();
+        playerAudioSource = gameObject.AddComponent<AudioSource>();
     }
 
     void Update()
     {
-        // x軸方向、z軸方向の入力を取得
-        // Horizontal（水平、横方向）
-        _input_x = Input.GetAxis("Horizontal");
-        // Vertical（垂直、縦方向
-        _input_z = Input.GetAxis("Vertical");
+        _inputx = Input.GetAxis("Horizontal");
+        _inputy = Input.GetAxis("Vertical");
 
-        Vector3 velocity = new Vector3(_input_x, 0, _input_z);
-        // ベクトルの向きを取得
+        // Y軸方向の入力を無視して、Z軸方向に進むように修正
+        Vector3 velocity = new Vector3(_inputx, 0, _inputy);
         Vector3 direction = velocity.normalized;
-        // 移動距離を計算
-        float distance = _speed * Time.deltaTime;
-        // 移動先を計算
-        Vector3 destination = transform.position + direction * distance;
-        // Rigidbodyに速度情報を伝えて移動
-        _rb.velocity = direction * _speed;
+        float distance = _speedPG * Time.deltaTime;
 
         // Rigidbodyに速度情報を伝えて移動
-        _rb.velocity = direction * _speed;
+        _rigidbody.velocity = new Vector3(direction.x * _speedPG, _rigidbody.velocity.y, direction.z * _speedPG);
+
         TurnPlayer();
         UpdateAnimator();
     }
 
-    // 左右の向きの調整
     void TurnPlayer()
     {
-        if (_input_x > 0)
+        if (_inputx > 0)
         {
             _playerTransform.localScale = new Vector3(-1, 1, 1);
         }
-        else if (_input_x < 0)
+        else if (_inputx < 0)
         {
             _playerTransform.localScale = new Vector3(1, 1, 1);
         }
@@ -62,12 +149,9 @@ public class PlayerController : MonoBehaviour
 
     void UpdateAnimator()
     {
-        float moveSpeed = Mathf.Abs(_input_x) + Mathf.Abs(_input_z);
-
-        // キーボードが押されているかどうかを確認
+        float moveSpeed = Mathf.Abs(_inputx) + Mathf.Abs(_inputy);
         bool isWalking = moveSpeed > 0;
 
-        // アニメーションの制御
         _penguinAnimator.SetBool("Idle", !isWalking);
         _penguinAnimator.SetBool("Walk_Side", false);
         _penguinAnimator.SetBool("Walk_Front", false);
@@ -75,24 +159,20 @@ public class PlayerController : MonoBehaviour
 
         if (isWalking)
         {
-            if (_input_x < 0)
+            if (_inputx < 0)
             {
-                // 左に移動
                 _penguinAnimator.SetBool("Walk_Side", true);
             }
-            else if (_input_x > 0)
+            else if (_inputx > 0)
             {
-                // 右に移動
                 _penguinAnimator.SetBool("Walk_Side", true);
             }
-            else if (_input_z < 0)
+            else if (_inputy < 0)
             {
-                // 後ろに移動
                 _penguinAnimator.SetBool("Walk_Front", true);
             }
-            else if (_input_z > 0)
+            else if (_inputy > 0)
             {
-                // 前に移動
                 _penguinAnimator.SetBool("Walk_Back", true);
             }
         }
