e53386f4abfe6bac1af7201f982eff90e7a2485e
saitou-star <program0719.saitou@gmail.com>
2023-12-18 13:38:04 +0900

    LowPolyMedievalPropsLite metaファイル削除,SaveButton.cs 一部コメントアウト

--- Assets/machida/SaveButton.cs
+++ Assets/machida/SaveButton.cs
@@ -6,21 +6,21 @@ public class SaveButton : MonoBehaviour
 {
     public void SaveGame()
     {
-        GameData data = new GameData();
-        data.playerLevel = 10;
-        data.playerHealth = 100f;
+        // GameData data = new GameData();
+        // data.playerLevel = 10;
+        // data.playerHealth = 100f;
 
-        SaveManager.SaveGame(data);
+        // SaveManager.SaveGame(data);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
