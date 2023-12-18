6a7e5f3009fcb7f91c133de620cb4374e9cf9f93
yamasabu30 <barani25510@gmail.com>
2023-12-18 13:06:53 +0900

    受け渡し用フォルダ作った

--- Assets/machida/SaveSceneMove.cs
+++ Assets/machida/SaveSceneMove.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SaveButton : MonoBehaviour
+public class SaveSceneMove : MonoBehaviour
 {
     public void OnSaveButtonClick()
     {
