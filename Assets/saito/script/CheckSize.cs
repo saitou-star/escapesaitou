e5e7497ee2198ad12cfc017f11610a6cb71f954d
saitou-star <program0719.saitou@gmail.com>
2023-12-21 15:35:04 +0900

    バケツ進行。

--- Assets/saito/script/CheckSize.cs
+++ Assets/saito/script/CheckSize.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowTarget : MonoBehaviour
+public class Cheak : MonoBehaviour
 {
+    MeshRenderer Mesh;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mesh = GetComponent<MeshRenderer>();
+        Debug.Log(Mesh.bounds.size);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
