6304a26100d4db3cc35fc9ad9a119a234afaf7c5
Rina-24 <rina.k.game@gmail.com>
2023-12-23 00:56:27 +0900

    暖炉完成

--- Assets/komatsu/Scripts/FirePlace.cs
+++ Assets/komatsu/Scripts/FirePlace.cs
@@ -7,35 +7,38 @@ public class FirePlace : MonoBehaviour
 {
     
     bool isChange = false;
+
     [SerializeField]
     private  Image backgroundImage; // 最初の背景
     [SerializeField]
     private Sprite newBackgroundImage; // 新しい背景
+    [SerializeField]
+    private GameObject DownArrow; // 成功時に表示するUI
     
      int completeNum = 0;
-   
 
-    // 宝箱を開ける
+    // 画像を入れ替える
     public void ChangeBackground()
     {
+        Debug.Log("ChangeBackground called!"); // デバッグメッセージを追加
         if(isChange) return;
         isChange = true;
 
         Debug.Log("OnOpen");
         
         backgroundImage.sprite = newBackgroundImage;
+        DownArrow.SetActive(true);
         
     }
-
 
 
     public void OnUseItem()
     {
         completeNum++;
-
         if(completeNum >= 2)
         {
-            Debug.Log("成功。移動できます。");
+        ChangeBackground();
+        Debug.Log("成功。移動できます。");
         }
     }
 
