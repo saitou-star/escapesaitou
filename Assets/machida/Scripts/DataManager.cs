2ff1d8274556288d3a7774f82647ff2ccea227c8
yamasabu30 <barani25510@gmail.com>
2024-01-06 14:01:52 +0900

    6日途中

--- Assets/machida/Scripts/DataManager.cs
+++ Assets/machida/Scripts/DataManager.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
+    public static DataManager Instance { get; private set; }
     private Transform playerTransform;
     private ItemBox boxScript;
+    public Vector3 playerPosition;
     private void Awake()
     {
         // プレイヤー座標の為のオブジェクト取得
         GameObject playerObject = GameObject.Find("Penguin");
         // アイテムボックスの為のオブジェクト取得
-        GameObject boxObject = GameObject.Find("ItemBox");
+        GameObject boxObject = GameObject.Find("ItemBoxCanvas");
         boxScript = boxObject.GetComponent<ItemBox>();
 
         if (playerObject != null)
@@ -24,7 +26,7 @@ public class DataManager : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(playerTransform.position);
+        // Debug.Log(playerTransform.position);
 
         // boxScript.Slot.ToList().ForEach(System.Console.WriteLine);
 
