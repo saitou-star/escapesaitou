e16fa407187175390074c0bad857ecd4b32f7d81
saitou-star <program0719.saitou@gmail.com>
2024-01-10 13:27:51 +0900

    シーン移動途中

---
+++ Assets/saito/2/script/ExitManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitManager : MonoBehaviour
+{
+    public GameObject player;  // 接触してきたオブジェクト
+    public Canvas window;  // 表示・非表示対象となるcanvas
+
+    void Start()
+    {
+        // 初期で非アクティブ化
+        window.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            window.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+
+            }
+            else if (Input.GetKeyDown(KeyCode.N))
+            {
+
+            }
+
+
+        }
+    }
+}
