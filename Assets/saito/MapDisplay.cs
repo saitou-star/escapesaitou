dd08b7302fb3b27e6d8798dbe4eb52487642c39d
saitou-star <program0719.saitou@gmail.com>
2024-01-15 10:04:20 +0900

    メニュー画面結合

---
+++ Assets/saito/MapDisplay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
